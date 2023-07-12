f6fd2eaa63ad893f21b81f3290d96c8e6ee5ed6d
ramvrm <ramvrm5@gmail.com>
2023-07-12 16:30:30 +0530

    changes in portfoliopage

--- src/components/portfolio/PortfolioPage.jsx
+++ src/components/portfolio/PortfolioPage.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import HeaderSocials from '../header/HeaderSocials'
+import HeaderSocials from '../header/HeaderSocials';
 
 const PortfolioPage = () => {
     const location = useLocation();
