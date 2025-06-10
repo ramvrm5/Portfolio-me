1d815281450237f98dbc7aad0e81820a98a5d9eb
ramvrm5 <ramvrm5@gmail.com>
2025-06-10 16:46:14 +0530

    changes in portfolio part

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import { collection, doc, getDocs } from "firebase/firestore";
-import { db } from '../../firebase'
-import './portfolio.css';
+import { db } from "../../firebase";
+import "./portfolio.css";
 
 const Portfolio = () => {
   const navigate = useNavigate();
@@ -11,15 +11,15 @@ const Portfolio = () => {
 
   useEffect(() => {
     const getPortfolio = async () => {
-      const querySnapshot = await getDocs(collection(db, 'porfolio'));
-      setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
-    }
+      const querySnapshot = await getDocs(collection(db, "porfolio"));
+      setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
+    };
     getPortfolio();
-  }, [])
+  }, []);
 
   const handlePortfolioPage = () => {
-    navigate('/Portfolio', { state: { data: portfolio } });
-  }
+    navigate("/Portfolio", { state: { data: portfolio } });
+  };
 
   return (
     <section id="portfolio">
@@ -27,35 +27,52 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {
-          portfolio.map(({ id, image, title, github, demo }, i) => {
-            return (
-              <>
-                {(title === "Amazon-clone" || title === "Airbnb-clone" || title === "Disney") && (<article key={i} className="portfolio__item">
+        {portfolio.map(({ id, image, title, github, demo }, i) => {
+          return (
+            <>
+              {(title === "Amazon-clone" ||
+                title === "Airbnb-clone" ||
+                title === "Note-taking-app") && (
+                <article key={i} className="portfolio__item">
                   <div className="portfolio__item-image">
-                    <img src={image} alt="" />
+                    <img src={image} alt="" style={{ height: "150px" }} />
                     <h3>{title}</h3>
                     <div className="portfolio__item-cta">
-                      <a href={github} className="btn" target="_blank" rel="noreferrer">Github</a>
-                      <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+                      <a
+                        href={github}
+                        className="btn"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Github
+                      </a>
+                      <a
+                        href={demo}
+                        className="btn btn-primary"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Live Demo
+                      </a>
                     </div>
                   </div>
-                </article>)}
-              </>
-            )
-          })
-        }
+                </article>
+              )}
+            </>
+          );
+        })}
 
         <div></div>
         <div className="portfolio__button__container">
-
           {/* <Link to="/portfolioPage" className="btn btn-primary">View more</Link> */}
 
-          <a onClick={() => handlePortfolioPage()} className="btn btn-primary">View More</a>
+          <a onClick={() => handlePortfolioPage()} className="btn btn-primary">
+            View More
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- src/components/portfolio/PortfolioPage.jsx
+++ src/components/portfolio/PortfolioPage.jsx
@@ -1,54 +1,69 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import HeaderSocials from '../header/HeaderSocials';
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import HeaderSocials from "../header/HeaderSocials";
 
 const PortfolioPage = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const [portfolio, setPortfolio] = useState(location.state.data);
+  const [portfolio, setPortfolio] = useState(location.state.data);
 
-    const handleBackToHomePage = () => {
-        navigate('/');
-    }
+  const handleBackToHomePage = () => {
+    navigate("/");
+  };
 
-    return (
-        <div className="portfolio__page">
-            <a onClick={() => handleBackToHomePage()} className="btn btn-primary portfolio__page_back"> &larr; Back</a>
-            <section>
-                <div className='portfolio__page_main'>
-                    <div className="portfolio__page_social">
-                        <HeaderSocials />
-                    </div>
-                    <div className="portfolio__page_list">
-
-                        <div className="container portfolio__container">
-                            {
-                                portfolio.map(({ id, image, title, github, demo }, i) => {
-                                    return (
-                                        <>
-                                            <article key={i} className="portfolio__item">
-                                                <div className="portfolio__item-image">
-                                                    <img src={image} alt="" />
-                                                    <h3>{title}</h3>
-                                                    <div className="portfolio__item-cta">
-                                                        <a href={github} className="btn" target="_blank" rel="noreferrer">Github</a>
-                                                        <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-                                                    </div>
-                                                </div>
-                                            </article>
-                                        </>
-                                    )
-                                })
-                            }
+  return (
+    <div className="portfolio__page">
+      <a
+        onClick={() => handleBackToHomePage()}
+        className="btn btn-primary portfolio__page_back"
+      >
+        {" "}
+        &larr; Back
+      </a>
+      <section>
+        <div className="portfolio__page_main">
+          <div className="portfolio__page_social">
+            <HeaderSocials />
+          </div>
+          <div className="portfolio__page_list">
+            <div className="container portfolio__container">
+              {portfolio.map(({ id, image, title, github, demo }, i) => {
+                return (
+                  <>
+                    <article key={i} className="portfolio__item">
+                      <div className="portfolio__item-image">
+                        <img src={image} alt="" style={{ height: "150px" }} />
+                        <h3>{title}</h3>
+                        <div className="portfolio__item-cta">
+                          <a
+                            href={github}
+                            className="btn"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Github
+                          </a>
+                          <a
+                            href={demo}
+                            className="btn btn-primary"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Live Demo
+                          </a>
                         </div>
-                    </div>
-                </div>
-
-
-            </section>
+                      </div>
+                    </article>
+                  </>
+                );
+              })}
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
-export default PortfolioPage
+export default PortfolioPage;
